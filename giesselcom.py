6d0cf30e6e3d0dbdbb7d0867fc72135cd0555f8c
Andrew GIessel <andrew.giessel@gmail.com>
2012-12-16 10:21:10 -0500

    main flask file re-factor

--- giesselcom.py
+++ giesselcom.py
@@ -13,42 +13,26 @@ application = Flask(__name__)
 application.debug=True
 
 # templates
-
 # directories
+
+#    static/
+#      js/
+#      css/
+#      img/
 #    templates/
-#      page.html
-#      post.html
+#      blog.html
+#      blog_post.html
 #      research.html
+#      about.html
+#      contact.html
 #      404.html
 #    blog/
 #       year/
-#          post-name-like-this/
-#            post.md
+#          post-name-like-this.md
 #               title
-#               slug
 #               published (date | None)
 #               blank line
 #               md body
-#             content/
-#               pictures, pde files, etc.
-
-@application.route('/')
-def index():
-    # post = most recent blog post
-    
-    most_recent_year = os.walk('./blog').next()[1][::-1][0]
-    
-    # build dictionary of lists
-    # keys: years
-    # values: post data, sorted by date
-    blog_data = {}
-
-    post_dirs = os.walk(os.path.join('./blog', most_recent_year)).next()[1]
-    post_data = [parse_post_file(os.path.join('./blog', most_recent_year, post, 'index.md')) for post in post_dirs]
-    sorted_post_data = sorted(post_data, key=lambda post: post['published'])[::-1]   
-
-    return render_template('blog_post.html', post=sorted_post_data[0])
-
 
 @application.route('/about/')
 def about_index():
@@ -62,49 +46,63 @@ def research_index():
 def contact_index():
     return render_template('contact.html')
 
+@application.route('/')
+def index():
+    # get sorted list of years (newest first) and dictionary of all blog data
+    years, blog_data = parse_blog_directory()
+
+    most_recent_year = years[0]
+    most_recent_post = blog_data[most_recent_year][0]
+    
+    # render the most recent post
+    return render_template('blog_post.html', post=most_recent_post)
+
 @application.route('/blog/')
 def blog_index():
-    # list of all years
-    years = os.walk('./blog').next()[1]
-    
-    # build dictionary of lists
-    # keys: years
-    # values: post data, sorted by date
-    blog_data = {}
+    # get sorted list of years (newest first) and dictionary of all blog data
 
-    for year in years:
-        post_dirs = os.walk(os.path.join('./blog', year)).next()[1]
-        
-        post_data = [parse_post_file(os.path.join('./blog', year, post, 'index.md')) for post in post_dirs]
-        post_data = sorted(post_data, key=lambda post: post['published'])[::-1]   
-
-        blog_data[year] = post_data
-
-    return render_template('blog.html', blog_data=blog_data, years=years[::-1])
+    years, blog_data = parse_blog_directory()
+    return render_template('blog.html', blog_data=blog_data, years=years)
 
 @application.route('/blog/<postname>')
 def blog_post(postname=None):
-    ###blog_dir = './gcom/blog'
-    blog_dir = './blog'
-    dirs = os.walk('./blog').next()[1]
-    for d in dirs:
-        index_file = os.path.join(blog_dir, d, postname, 'index.md')
-        if os.path.isfile(index_file):
-            post = parse_post_file(index_file)
-            return render_template('blog_post.html', post=post)
+    years, blog_data = parse_blog_directory()
+    for year in years:
+        posts = blog_data[year]
+        for post in posts:
+            if post['url'] == postname:
+                return render_template('blog_post.html', post=post, change_title=True)
 
     # if we didn't find the post, throw a 404
     return render_template('404.html'), 404
 
-def parse_post_file(filename):
-    file = open(filename)
-    lines = file.readlines()
-    file.close()
+def parse_blog_directory():
+    all_years = os.walk('./blog').next()[1]
+    
+    # build dictionary of lists
+    # year: [sorted post data]
+    blog_data = {}
 
+    for year in all_years:
+        post_data = [parse_post_file(md_file) for md_file in glob.glob('./blog/' + year + '/*.md')]
+        sorted_post_data = sorted(post_data, key=lambda post: post['published'])[::-1]
+
+        # remove any post where "published" is False (aka a draft)
+        sorted_post_data = [post for post in sorted_post_data if post['published']]
+
+        if sorted_post_data:
+            blog_data[year] = sorted_post_data
+
+    # can get this from the reverse sorted keys of the blog_data dict
+    valid_years = sorted(blog_data)[::-1]
+    
+    return valid_years, blog_data
+
+def parse_post_file(filename):
+    lines = open(filename).readlines()
     post = {}
 
     # Read lines until an empty line is encountered, then build a dictionary with yaml
-    
     # code jacked from flask-flatpages
     lines = iter(lines)
     meta = u'\n'.join(itertools.takewhile(string.strip, lines))
@@ -112,11 +110,11 @@ def parse_post_file(filename):
 
     # The rest is the content. `lines` is an iterator so it continues
     # where `itertools.takewhile` left it.
-    post['filename'] = filename
-    post['dirname'] = os.path.dirname(filename).split('/')[-1]
     post['body'] = u'\n'.join(lines)
     post['body'] = Markup(markdown.markdown(post['body']))
 
+    post['filename'] = filename
+    post['url'] = os.path.basename(filename).split('.')[0]
     return post
 
 if __name__ == '__main__':
